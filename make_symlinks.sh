b41177e9c9b42301b73a6e76cabfc96e3bde67d7
lumbric (antares) <lumbric@gmail.com>
2015-05-02 13:29:12 +0200

    improve install script

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -1,4 +1,31 @@
-#!/bin/sh
+#!/bin/bash
+#
+# Run this script in kossel_config repo to create symlinks in your home
+# directory to the config files.
 
-# run this script to add all symlinks to this config files
-ln -s $PWD/printrun/.pronsolerc $HOME/.pronsolerc
+
+ln_to_home() {
+    # create symlink with absolute paths from current directory to home
+    # directory and ask if overwrite
+    source=$1
+    target=$2
+
+    echo "Creating symlink $PWD/$source -> $HOME/$target..."
+    ln -i -s $PWD/$source $HOME/$target
+}
+
+
+# Printrun
+ln_to_home printrun/.pronsolerc .pronsolerc
+
+
+# Slic3r
+# Note: don't use this double loop if you make config_templates not existing
+# for all three folders.
+CONFIG_TEMPLATES=("rubinstein")
+FOLDERS=("filament" "print" "printer")
+for config in ${CONFIG_TEMPLATES[@]}; do
+    for folder in ${FOLDERS[@]}; do
+        ln_to_home slic3r/$folder/${config}.ini .Slic3r/$folder/${config}.ini
+    done
+done
